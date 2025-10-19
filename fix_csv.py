67798eb7007f5881c57ae12f0d1a5d0d4abff04d
yoru <jordanchandra1@gmail.com>
2025-10-19 21:06:35 +0700

    .

--- fix_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-file_path = 'XAUUSDc_M1_202506111341_202509220003.csv'
-df = pd.read_csv(file_path, sep='\t')
-
-out_path = 'XAUUSDx_M1.csv'
-df.to_csv(out_path, index=False)
-out_path
